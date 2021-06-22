f074f8869cbd296d6d22363144d8da19e87d1384
Haydar Bekmuradov <haydarbekmuradov@mail.ru>
2021-06-22 20:40:23 +0300

    test

--- vkForFlypika4/AppDelegate.swift
+++ vkForFlypika4/AppDelegate.swift
@@ -10,20 +10,20 @@ import VK_ios_sdk
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, AuthServiceDelegate {
-
+    
     
     // window
-var window: UIWindow?
-
+    var window: UIWindow?
+    
     var authService: AuthService!
-
+    
     static func shared() -> AppDelegate {
-     return UIApplication.shared.delegate as! AppDelegate
-
+        return UIApplication.shared.delegate as! AppDelegate
+        
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-window = UIWindow()
+        window = UIWindow()
         self.authService = AuthService()
         authService.delegate = self
         let authVC = UIStoryboard(name: "AuthViewController", bundle: nil).instantiateInitialViewController() as! AuthViewController
@@ -39,21 +39,21 @@ window = UIWindow()
     func authServiceShouldShow(_ viewController: UIViewController) {
         print(#function)
         window?.rootViewController?.present(viewController, animated: true, completion: nil)
-
+        
     }
     
     func authServiceSignIn() {
         
         let feedVC = UIStoryboard(name: "FeedViewController", bundle: nil).instantiateInitialViewController() as! UITabBarController
-//        let navVC = UINavigationController(rootViewController: feedVC)
+        //        let navVC = UINavigationController(rootViewController: feedVC)
         window?.rootViewController = feedVC
         print(#function)
-
+        
     }
     
     func authServiceDidSignInFail() {
         print(#function)
-
+        
     }
 }
 
--- vkForFlypika4/service/Api.swift
+++ vkForFlypika4/service/Api.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct API {
+    
     //https://api.vk.com/method/newsfeed.get?filters=post&access_token=502f3bbe76cc153fc888ab27f2603de90cce5988c24a6ca72b2a8e571c34964c83ff988cf95c59481d0f8&v=5.131
 
     static let scheme = "https"
@@ -17,4 +18,5 @@ struct API {
     static let version = "5.131"
     
 }
-// https://api.vk.com/method/newsfeed.get?v=5.131&access_token=502f3bbe76cc153fc888ab27f2603de90cce5988c24a6ca72b2a8e571c34964c83ff988cf95c59481d0f8&filters=post,photo
+
+//https://api.vk.com/method/newsfeed.get?v=5.131&access_token=502f3bbe76cc153fc888ab27f2603de90cce5988c24a6ca72b2a8e571c34964c83ff988cf95c59481d0f8&filters=post,photo
